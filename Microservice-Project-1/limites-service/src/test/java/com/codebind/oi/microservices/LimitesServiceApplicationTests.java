5f64536e9c31473040e01fbd4abf3bd571bf5226
Codebind001 <joy.roy.nil76@gmail.com>
2021-03-18 13:25:17 +0600

    initial commit

---
+++ Microservice-Project-1/limites-service/src/test/java/com/codebind/oi/microservices/LimitesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebind.oi.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
