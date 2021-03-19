c0ec0b15c2a4db18ab84e46725cb720b5cb16d70
Codebind001 <joy.roy.nil76@gmail.com>
2021-03-19 12:31:02 +0600

    update git config file

--- Microservice-Project-1/limites-service/src/main/java/com/codebind/oi/microservices/LimitesConfigurationController.java
+++ Microservice-Project-1/limites-service/src/main/java/com/codebind/oi/microservices/LimitesConfigurationController.java
@@ -1,5 +1,6 @@
 package com.codebind.oi.microservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,14 @@ import com.codebind.oi.microservices.bean.LimiteConfiguration;
 @RestController
 public class LimitesConfigurationController {
 	
+	@Autowired
+	Configuration configuration;
+	
+	
 	@GetMapping("/limits")
 	public LimiteConfiguration retriveLimitsFromConfiguration() {
-		return new LimiteConfiguration(999,1);
+		 //LimiteConfiguration limiteConfiguration = 
+		 return new LimiteConfiguration(configuration.getMaximum(),configuration.getMinimum());
 	}
 
 }
